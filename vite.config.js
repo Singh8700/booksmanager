1168c45da871073b0d2f809baf8d598e3998cbf5
Singh8700 <rohit.si8700@gmail.com>
2023-06-30 16:13:14 +0530

    first-commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ var vite_1 = require("vite");
 var plugin_react_1 = require("@vitejs/plugin-react");
 // https://vitejs.dev/config/
 exports.default = (0, vite_1.defineConfig)({
-    base: '/booksmanager',
+    base: '/booksmanager/',
     plugins: [(0, plugin_react_1.default)()],
 });
